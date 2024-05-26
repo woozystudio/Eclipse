6d7e030f135ffb41d38b624a012d3c793ed9a445
Woozy's Studio <woozysstudio23@gmail.com>
2024-05-26 08:16:12 -0600

    feat: add canary and developers fields

--- interfaces/Configuration.ts
+++ interfaces/Configuration.ts
@@ -3,4 +3,7 @@ export default interface Configuration {
     token: string;
     botId: string;
     guildId: string;
+    canaryToken: string;
+    canaryBotId: string;
+    developers: string[];
 }
