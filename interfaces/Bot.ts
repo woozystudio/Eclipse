9661806c72608fd51012def17917145cb95b7423
Woozy's Studio <woozysstudio23@gmail.com>
2024-05-25 20:22:15 -0600

    feat: add command and subcommand

--- interfaces/Bot.ts
+++ interfaces/Bot.ts
@@ -1,8 +1,12 @@
 import { Collection } from "discord.js";
 import Configuration from "./Configuration";
+import Command from "../class/Command";
+import SubCommand from "../class/SubCommand";
 
 export default interface Bot {
     config: Configuration;
+    commands: Collection<string, Command>;
+    subCommands: Collection<string, SubCommand>;
 
     Init(): void;
     AddListeners(): void;
