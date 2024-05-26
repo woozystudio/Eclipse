4c51fef221c9d2a6da8d3ece473465560f39eeaa
Woozy's Studio <woozysstudio23@gmail.com>
2024-05-25 20:21:18 -0600

    feat: create command class

---
+++ class/Command.ts
@@ -0,0 +1,26 @@
+import { ChatInputCommandInteraction, CacheType, AutocompleteInteraction } from "discord.js";
+import CommandListener from "../interfaces/CommandListener";
+import Eclipse from "./Eclipse";
+import CommandManager from "../interfaces/CommandManager";
+
+export default class Command implements CommandListener {
+    client: Eclipse;
+    name: string;
+    description: string;
+    options: object;
+    default_member_permissions: bigint;
+
+    constructor(client: Eclipse, on: CommandManager) {
+        this.client = client;
+        this.name = on.name;
+        this.description = on.description;
+        this.options = on.options;
+        this.default_member_permissions = on.userPermissions;
+    }
+
+    Execute(interaction: ChatInputCommandInteraction<CacheType>): void {
+    }
+    AutoComplete(interaction: AutocompleteInteraction<CacheType>): void {
+    }
+    
+}
