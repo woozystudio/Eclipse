44d86d2081691a721564f9853e70095de928dd0d
Woozy's Studio <woozysstudio23@gmail.com>
2024-05-25 20:22:46 -0600

    feat: create interaction create event

---
+++ events/discord/interactionCreate/InteractionCreate.ts
@@ -0,0 +1,31 @@
+import { ChatInputCommandInteraction, Collection, Events } from "discord.js";
+import Event from "../../../class/Event";
+import Command from "../../../class/Command";
+import Eclipse from "../../../class/Eclipse";
+
+export default class InteractionCreate extends Event {
+    constructor(client: Eclipse) {
+        super(client, {
+            name: Events.InteractionCreate,
+            description: "InteractionCreate Event",
+            once: false
+        });
+    }
+
+    Execute(interaction: ChatInputCommandInteraction) {
+        if(!interaction.isChatInputCommand()) return;
+
+        const command: Command = this.client.commands.get(interaction.commandName)!;
+
+        //@ts-ignore
+        if(!command) return interaction.reply({ content: "outdated command", ephemeral: true }) && this.client.commands.delete(interaction.commandName);
+        try {
+            const subCommandGroup = interaction.options.getSubcommandGroup(false);
+            const subCommand = `${interaction.commandName}${subCommandGroup ? `.${subCommandGroup}`: ""}.${interaction.options.getSubcommand(false) || ""}`
+
+            return this.client.subCommands.get(subCommand)?.Execute(interaction) || command.Execute(interaction);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
