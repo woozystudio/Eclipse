e5b063c462ebbb6d56471f024278bb357277fc01
Woozy's Studio <woozysstudio23@gmail.com>
2024-05-26 08:14:37 -0600

    feat: add subcommand test

--- commands/TestCommand.ts
+++ commands/TestCommand.ts
@@ -1,18 +1,31 @@
-import { ChatInputCommandInteraction, EmbedBuilder, PermissionFlagsBits } from "discord.js";
+import { ApplicationCommandOptionType, ChatInputCommandInteraction, EmbedBuilder, PermissionFlagsBits } from "discord.js";
 import Eclipse from "../class/Eclipse";
 import Command from "../class/Command";
+import TestSubCommand from "../subCommands/SubCommand";
 
-export default class BotInfoCommand extends Command {
+export default class TestCommand extends Command {
     constructor(client: Eclipse) {
         super(client, {
             name: "test",
             description: "test",
             userPermissions: PermissionFlagsBits.UseApplicationCommands,
-            options: [],
+            options: [
+                {
+                    name: new TestSubCommand(client).group,
+                    description: "test",
+                    type: ApplicationCommandOptionType.SubcommandGroup,
+                    options: [
+                        {
+                            name: new TestSubCommand(client).name,
+                            description: "test",
+                            type: ApplicationCommandOptionType.Subcommand,
+                        }
+                    ]
+                }
+            ],
+            development: true
         });
     }
 
-    async Execute(interaction: ChatInputCommandInteraction) {
-        await interaction.reply("Test")
-    }
+    async Execute(interaction: ChatInputCommandInteraction) {}
 }
