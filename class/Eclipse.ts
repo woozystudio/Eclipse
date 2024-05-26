8e66ad4302d088370e8563155f7cd7fff0ad42a5
Woozy's Studio <woozysstudio23@gmail.com>
2024-05-26 08:13:43 -0600

    feat: add development and subcommand support

--- class/Eclipse.ts
+++ class/Eclipse.ts
@@ -12,6 +12,7 @@ export default class Eclipse extends Client implements Bot
     config: Configuration;
     commands: Collection<string, Command>;
     subCommands: Collection<string, SubCommand>;
+    development: boolean;
 
     constructor()
     {
@@ -28,15 +29,19 @@ export default class Eclipse extends Client implements Bot
         this.addListeners = new Listeners(this);
         this.commands = new Collection();
         this.subCommands = new Collection();
+        this.development = (process.argv.slice(2).includes("--development"));
     }
     
     Init(): void {
+        console.log(`Starting the client in ${this.development ? "the canary version." : "production mode."}`.cyan)
         this.AddListeners();
-        this.login(this.config.token).catch((err) => console.error(err));
+
+        this.login(this.development ? this.config.canaryToken : this.config.token).catch((err) => console.error(err));
     }
 
     AddListeners(): void {
         this.addListeners.createEventsListener();
         this.addListeners.createCommandsListener();
+        this.addListeners.createSubCommandsListener();
     }
 }
