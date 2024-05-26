8802d4158e0acf3e42f89d5af1dcaa7af49dcbe1
Woozy's Studio <woozysstudio23@gmail.com>
2024-05-26 08:40:04 -0600

    fix: update interaction create event

--- events/discord/interactionCreate/InteractionCreate.ts
+++ events/discord/interactionCreate/InteractionCreate.ts
@@ -13,23 +13,23 @@ export default class InteractionCreate extends Event {
     }
 
     Execute(interaction: ChatInputCommandInteraction) {
-        if(!interaction.isChatInputCommand()) return;
-
-        const command: Command = this.client.commands.get(interaction.commandName)!;
-
-        //@ts-ignore
-        if(!command) return interaction.reply({ content: "outdated command", ephemeral: true }) && this.client.commands.delete(interaction.commandName);
-
-        if(command.development && !this.client.config.developers.includes(interaction.user.id))
-            return interaction.reply({ content: `This command is only for developers!`, ephemeral: true });
-
-        try {
-            const subCommandGroup = interaction.options.getSubcommandGroup(false);
-            const subCommand = `${interaction.commandName}${subCommandGroup ? `.${subCommandGroup}`: ""}.${interaction.options.getSubcommand(false) || ""}`
-
-            return this.client.subCommands.get(subCommand)?.Execute(interaction) || command.Execute(interaction);
-        } catch (error) {
-            console.log(error);
+        if(interaction.isChatInputCommand()) {
+            const command: Command = this.client.commands.get(interaction.commandName)!;
+    
+            //@ts-ignore
+            if(!command) return interaction.reply({ content: "outdated command", ephemeral: true }) && this.client.commands.delete(interaction.commandName);
+    
+            if(command.development && !this.client.config.developers.includes(interaction.user.id))
+                return interaction.reply({ content: `This command is only for developers!`, ephemeral: true });
+    
+            try {
+                const subCommandGroup = interaction.options.getSubcommandGroup(false);
+                const subCommand = `${interaction.commandName}${subCommandGroup ? `.${subCommandGroup}`: ""}.${interaction.options.getSubcommand(false) || ""}`
+    
+                return this.client.subCommands.get(subCommand)?.Execute(interaction) || command.Execute(interaction);
+            } catch (error) {
+                console.log(error);
+            }
         }
     }
 }
