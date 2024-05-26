7fb29060d9214a483e519a35de203118a28ebdb7
Woozy's Studio <woozysstudio23@gmail.com>
2024-05-26 08:15:20 -0600

    feat: delete guild commands and create global and development commands

--- events/runtime/GuildCommandsLoader.ts
+++ events/runtime/GuildCommandsLoader.ts
@@ -14,15 +14,22 @@ export default class Ready extends Event {
     }
 
     async Execute() {
-        const clientId = this.client.config.botId;
-        const guildId = this.client.config.guildId;
+        const clientId = this.client.development ? this.client.config.canaryBotId : this.client.config.botId;
         const rest = new REST().setToken(this.client.config.token);
 
-        const guildCommands: any = await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
-            body: this.GetJson(this.client.commands.filter(command => command))
+        if(!this.client.development) {
+            const globalCommands: any = await rest.put(Routes.applicationCommands(clientId), {
+                body: this.GetJson(this.client.commands.filter(command => !command.development))
+            });
+
+            console.log(`Successfully loaded ${globalCommands.length} global application commands.`.yellow)
+        }
+
+        const devCommands: any = await rest.put(Routes.applicationGuildCommands(clientId, this.client.config.guildId), {
+            body: this.GetJson(this.client.commands.filter(command => command.development))
         });
 
-        console.log(`Successfully loaded ${guildCommands.length} guild application commands.`.red)
+        console.log(`Successfully loaded ${devCommands.length} development application commands.`.red)
     }
 
     private GetJson(commands: Collection<string, Command>): object[] {
