d59ac8d21cb9cefb659661223ce9d69e8ba47015
Woozy's Studio <woozysstudio23@gmail.com>
2024-05-27 17:59:41 -0600

    feat: apply category for the test command

--- commands/TestCommand.ts
+++ commands/TestCommand.ts
@@ -3,12 +3,14 @@ import Command from "../class/Command";
 import Eclipse from "../class/Eclipse";
 import MessageColor from "../enums/MessageColor";
 import Textures from "../enums/Textures";
+import Category from "../enums/Category";
 
 export default class TestCommand extends Command {
     constructor(client: Eclipse) {
         super(client, {
             name: "test",
             description: "Test Command",
+            category: Category.Test,
             userPermissions: PermissionFlagsBits.UseApplicationCommands,
             options: [],
             development: true
