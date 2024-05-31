8aa62d2b38f0328e78a2d721dbca25a6e26d0c40
Woozy's Studio <woozysstudio23@gmail.com>
2024-05-31 11:36:01 -0600

    feat: userPermissions in buttons (#3)

--- events/discord/interactionCreate/InteractionButtonCreate.ts
+++ events/discord/interactionCreate/InteractionButtonCreate.ts
@@ -12,12 +12,17 @@ export default class InteractionButtonCreate extends Event {
         });
     }
 
-    Execute(interaction: ButtonInteraction) {
+    async Execute(interaction: ButtonInteraction) {
         if (interaction.isButton()) {
             const button: Button = this.client.buttons.get(interaction.customId)!;
 
             //@ts-ignore
             if (!button) return interaction.reply({ content: `outdated button` }) && this.client.buttons.delete(interaction.customid);
+
+            const target = await interaction.guild?.members.fetch(interaction.user.id);
+
+            if(!target?.permissions.has(button.default_member_permissions)) return await interaction.reply({ content: "`❌` You don't have sufficient permissions to execute this button.", ephemeral: true });
+
             try {
                 const buttonId = `${interaction.customId}`;
     
