00ec74cb5813d7c8eef8121108ab01617e0d68fe
Woozy's Studio <woozysstudio23@gmail.com>
2024-05-31 11:39:40 -0600

    feat: move buttons and contextmenu interactions to the main event

--- events/discord/interactionCreate/InteractionCreate.ts
+++ events/discord/interactionCreate/InteractionCreate.ts
@@ -1,7 +1,9 @@
-import { ChatInputCommandInteraction, Collection, Events } from "discord.js";
+import { ButtonInteraction, ChatInputCommandInteraction, Collection, ContextMenuCommandInteraction, Events } from "discord.js";
 import Event from "../../../class/Event";
 import Command from "../../../class/Command";
 import Eclipse from "../../../class/Eclipse";
+import ContextMenu from "../../../class/ContextMenu";
+import Button from "../../../class/Button";
 
 export default class InteractionCreate extends Event {
     constructor(client: Eclipse) {
@@ -12,7 +14,7 @@ export default class InteractionCreate extends Event {
         });
     }
 
-    Execute(interaction: ChatInputCommandInteraction) {
+    async Execute(interaction: ChatInputCommandInteraction, contextMenuInteraction: ContextMenuCommandInteraction, buttonInteraction: ButtonInteraction) {
         if(interaction.isChatInputCommand()) {
             const command: Command = this.client.commands.get(interaction.commandName)!;
     
@@ -29,6 +31,47 @@ export default class InteractionCreate extends Event {
                 return this.client.subCommands.get(subCommand)?.Execute(interaction) || command.Execute(interaction);
             } catch (error) {
                 console.log(error);
+            }
+        }
+
+        if (contextMenuInteraction.isContextMenuCommand()) {
+            const contextMenu: ContextMenu = this.client.contextMenus.get(contextMenuInteraction.commandName)!;
+
+            if(contextMenu.development && !this.client.config.developers.includes(contextMenuInteraction.user.id))
+                return contextMenuInteraction.reply({ content: `This command is only for developers!`, ephemeral: true });
+
+            //@ts-ignore
+            if (!contextMenu) return contextMenuInteraction.reply({ content: `outdated menu` }) && this.client.contextMenus.delete(contextMenuInteraction.commandName);
+
+            const target = await contextMenuInteraction.guild?.members.fetch(contextMenuInteraction.user.id);
+
+            if(!target?.permissions.has(contextMenu.userPermissions)) return await contextMenuInteraction.reply({ content: "`❌` You don't have sufficient permissions to execute this command.", ephemeral: true });
+
+            try {
+                const context = `${contextMenuInteraction.commandName}${contextMenuInteraction.commandType}`
+
+                return this.client.contextMenus.get(context)?.Execute(contextMenuInteraction) || contextMenu.Execute(contextMenuInteraction);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+
+        if (buttonInteraction.isButton()) {
+            const button: Button = this.client.buttons.get(buttonInteraction.customId)!;
+
+            //@ts-ignore
+            if (!button) return buttonInteraction.reply({ content: `outdated button` }) && this.client.buttons.delete(buttonInteraction.customid);
+
+            const target = await buttonInteraction.guild?.members.fetch(buttonInteraction.user.id);
+
+            if(!target?.permissions.has(button.default_member_permissions)) return await buttonInteraction.reply({ content: "`❌` You don't have sufficient permissions to execute this button.", ephemeral: true });
+
+            try {
+                const buttonId = `${buttonInteraction.customId}`;
+    
+                this.client.buttons.get(buttonId)?.Execute(buttonInteraction) || button.Execute(buttonInteraction);
+            } catch (err) {
+                console.log(err);
             }
         }
     }
