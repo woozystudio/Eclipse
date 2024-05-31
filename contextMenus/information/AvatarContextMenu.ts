17e063e113106aac6976d5463954c4de86c4e1a6
Woozy's Studio <woozysstudio23@gmail.com>
2024-05-31 10:56:07 -0600

    feat: avatar context menu command

---
+++ contextMenus/information/AvatarContextMenu.ts
@@ -0,0 +1,27 @@
+import { ApplicationCommandType, CacheType, ContextMenuCommandInteraction, EmbedBuilder, UserContextMenuCommandInteraction } from "discord.js";
+import ContextMenu from "../../class/ContextMenu";
+import Eclipse from "../../class/Eclipse";
+import Category from "../../enums/Category";
+
+export default class AvatarMenu extends ContextMenu {
+    constructor(client: Eclipse) {
+        super(client, {
+            name: "Avatar",
+            type: ApplicationCommandType.User,
+            category: Category.Information,
+            development: true
+        });
+    }
+
+    async Execute(interaction: UserContextMenuCommandInteraction) {
+        const target = interaction.targetUser;
+
+        const AvatarEmbed = new EmbedBuilder()
+        .setColor(0x2B2D31)
+        .setTitle(`@${target?.username}'s avatar`)
+        .setImage(`${target?.displayAvatarURL({ size: 4096 })}`)
+        .setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() })
+
+        await interaction.reply({ embeds: [AvatarEmbed] });
+    }
+}
