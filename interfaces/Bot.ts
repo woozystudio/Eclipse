f0c6f9c68676c48bfe1c434b334f6b0a8082278c
Woozy's Studio <woozysstudio23@gmail.com>
2024-05-31 18:02:10 -0600

    fix: matching the interface and the bot class

--- interfaces/Bot.ts
+++ interfaces/Bot.ts
@@ -2,12 +2,19 @@ import { Collection } from "discord.js";
 import Configuration from "./Configuration";
 import Command from "../class/Command";
 import SubCommand from "../class/SubCommand";
+import Listeners from "../class/Listeners";
+import Button from "../class/Button";
+import ContextMenu from "../class/ContextMenu";
 
 export default interface Bot {
+    addListeners: Listeners;
     config: Configuration;
     commands: Collection<string, Command>;
     subCommands: Collection<string, SubCommand>;
+    buttons: Collection<string, Button>;
+    contextMenus: Collection<string, ContextMenu>
     development: boolean;
+    version: any;
 
     Login(): void;
     AddListeners(): void;
