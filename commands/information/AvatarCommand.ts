a9d3a89b13c0660fab6a79ccb7eb123516ecbf65
Woozy's Studio <woozysstudio23@gmail.com>
2024-06-02 16:56:22 -0600

    feat: add size options (#2)

--- commands/information/AvatarCommand.ts
+++ commands/information/AvatarCommand.ts
@@ -16,20 +16,72 @@ export default class Avatar extends Command {
                     description: "Select a user from the server to display their avatar.",
                     required: true,
                     type: ApplicationCommandOptionType.User
+                },
+                {
+                    name: "size",
+                    description: "Select a size for the avatar image.",
+                    required: false,
+                    type: ApplicationCommandOptionType.String,
+                    choices: [
+                        { name: '16', value: '16' },
+                        { name: '32', value: '32' },
+                        { name: '64', value: '64' },
+                        { name: '128', value: '128' },
+                        { name: '256', value: '256' },
+                        { name: '512', value: '512' },
+                        { name: '1024', value: '1024' },
+                        { name: '2048', value: '2048' },
+                        { name: '4096', value: '4096' },
+                    ]
                 }
             ],
-            development: false
+            development: true
         });
     }
 
     async Execute(interaction: ChatInputCommandInteraction) {
         const target = interaction.options.getUser('target') as User;
+        const size = interaction.options.getString('size') || 4096;
 
         const AvatarEmbed = new EmbedBuilder()
         .setColor(0x2B2D31)
         .setTitle(`@${target?.username}'s avatar`)
-        .setImage(`${target?.displayAvatarURL({ size: 4096 })}`)
         .setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() })
+        
+        switch (size) {
+            case '16':
+                AvatarEmbed.setImage(`${target?.displayAvatarURL({ size: 16 })}`)
+                break;
+            case '32':
+                AvatarEmbed.setImage(`${target?.displayAvatarURL({ size: 32 })}`)
+                break;
+            case '64':
+                AvatarEmbed.setImage(`${target?.displayAvatarURL({ size: 64 })}`)
+                break;
+            case '128':
+                AvatarEmbed.setImage(`${target?.displayAvatarURL({ size: 128 })}`)
+                break;
+            case '256':
+                AvatarEmbed.setImage(`${target?.displayAvatarURL({ size: 256 })}`)
+                break;
+            case '512':
+                AvatarEmbed.setImage(`${target?.displayAvatarURL({ size: 512 })}`)
+                break;
+            case '1024':
+                AvatarEmbed.setImage(`${target?.displayAvatarURL({ size: 1024 })}`)
+                break;
+            case '2048':
+                AvatarEmbed.setImage(`${target?.displayAvatarURL({ size: 2048 })}`)
+                break;
+            case '4096':
+                AvatarEmbed.setImage(`${target?.displayAvatarURL({ size: 4096 })}`)
+                break;
+
+            default:
+                AvatarEmbed.setImage(`${target?.displayAvatarURL({ size: 4096 })}`)
+                break;
+        }
+
 
         await interaction.reply({ embeds: [AvatarEmbed] });
     }
