7413701ed75b08c1b072bebef5374d233ea73b54
Woozy's Studio <woozysstudio23@gmail.com>
2024-06-08 15:05:42 -0600

    feat: config command

---
+++ commands/configuration/ConfigurationCommand.ts
@@ -0,0 +1,34 @@
+import { ApplicationCommandOptionType, ChannelType, ChatInputCommandInteraction, PermissionFlagsBits } from "discord.js";
+import Command from "../../class/Command";
+import Eclipse from "../../class/Eclipse";
+import Category from "../../enums/Category";
+
+export default class Configuration extends Command {
+    constructor(client: Eclipse) {
+        super(client, {
+            name: "config",
+            description: "Configure all bot plugins with this command.",
+            category: Category.Configuration,
+            userPermissions: PermissionFlagsBits.ManageGuild,
+            options: [
+                {
+                    name: "reports",
+                    description: "Configure the reporting plugin.",
+                    type: ApplicationCommandOptionType.Subcommand,
+                    options: [
+                        {
+                            name: "channel",
+                            description: "Select the channel where the reports will be sent.",
+                            required: true,
+                            type: ApplicationCommandOptionType.Channel,
+                            channel_types: [ChannelType.GuildText]
+                        }
+                    ]
+                }
+            ],
+            development: false
+        });
+    }
+
+    async Execute(interaction: ChatInputCommandInteraction) {}
+}
