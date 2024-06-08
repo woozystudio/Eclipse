cdd50119ac010ad17102522baecfa0a7f397e69e
Woozy's Studio <woozysstudio23@gmail.com>
2024-06-08 15:06:07 -0600

    feat: reports subcommand

---
+++ subCommands/configuration/ReportsSubCommand.ts
@@ -0,0 +1,24 @@
+import { ChatInputCommandInteraction, TextChannel, inlineCode } from "discord.js";
+import SubCommand from "../../class/SubCommand";
+import Eclipse from "../../class/Eclipse";
+import Category from "../../enums/Category";
+import ReportsConfig from "../../database/ReportsConfig";
+
+export default class Reports extends SubCommand {
+    constructor(client: Eclipse) {
+        super(client, {
+            name: "reports",
+            category: Category.Configuration,
+            command: "config",
+            group: ""
+        });
+    }
+
+    async Execute(interaction: ChatInputCommandInteraction) {
+        const channel = interaction.options.getChannel('channel') as TextChannel;
+        
+        await ReportsConfig.findOneAndUpdate({ GuildID: interaction.guild?.id }, { ChannelID: channel.id, }, { new: true, upsert: true });
+
+        await interaction.reply({ content: `${inlineCode('✅')} The reporting plugin has been successfully configured.` });
+    }
+}
