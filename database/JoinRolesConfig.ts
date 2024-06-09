6632e8a43c2b5599ee354ae020e57ad1c8473a4c
Woozy's Studio <woozysstudio23@gmail.com>
2024-06-09 13:06:26 -0600

    feat: set roleid to array

--- database/JoinRolesConfig.ts
+++ database/JoinRolesConfig.ts
@@ -2,7 +2,7 @@ import { Schema, model } from "mongoose";
 
 let joinRolesSchema = new Schema({
     GuildID: String,
-    RoleID: String,
+    RoleID: [String],
 });
 
 export default model('JoinRolesConfig', joinRolesSchema);
