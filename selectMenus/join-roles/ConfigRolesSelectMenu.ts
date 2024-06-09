927e9ab58255807b96f839ae8af178356231da6b
Woozy's Studio <woozysstudio23@gmail.com>
2024-06-09 14:15:39 -0600

    feat: create roles selectmenu

---
+++ selectMenus/join-roles/ConfigRolesSelectMenu.ts
@@ -0,0 +1,20 @@
+import { AnySelectMenuInteraction, PermissionFlagsBits, inlineCode } from "discord.js";
+import Eclipse from "../../class/Eclipse";
+import SelectMenu from "../../class/SelectMenu";
+import JoinRolesConfig from "../../database/JoinRolesConfig";
+
+export default class JoinRolesConfigSelectMenu extends SelectMenu {
+    constructor(client: Eclipse) {
+        super(client, {
+            customId: "joinroles-config-menu",
+            userPermissions: PermissionFlagsBits.ManageGuild
+        })
+    }
+    
+    async Execute(interaction: AnySelectMenuInteraction) {
+        const selectedRoles = interaction.values;
+        await JoinRolesConfig.findOneAndUpdate({ GuildID: interaction.guild?.id }, { RoleID: selectedRoles, }, { new: true, upsert: true });
+
+        await interaction.reply({ content: `${inlineCode('✅')} The join-roles plugin has been successfully configured.`, ephemeral: true });
+    }
+}
