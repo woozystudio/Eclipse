9acdf1c4baac0c6b63234a676d3dd6b12cd9c049
Woozy's Studio <woozysstudio23@gmail.com>
2024-06-15 16:07:48 -0600

    feat: lock & unlock command
    Signed-off-by: Woozy's Studio <woozysstudio23@gmail.com>

--- commands/moderation/UnlockCommand.ts
+++ commands/moderation/UnlockCommand.ts
@@ -3,17 +3,17 @@ import Command from "../../class/Command";
 import Eclipse from "../../class/Eclipse";
 import Category from "../../enums/Category";
 
-export default class Lock extends Command {
+export default class Unlock extends Command {
     constructor(client: Eclipse) {
         super(client, {
-            name: "lock",
-            description: "Locks a channel for server users.",
+            name: "unlock",
+            description: "Unlocks a channel for server users.",
             category: Category.Moderation,
             userPermissions: PermissionFlagsBits.ManageChannels,
             options: [
                 {
                     name: "channel",
-                    description: "Select a channel to lock.",
+                    description: "Select a channel to unlock.",
                     required: false,
                     type: ApplicationCommandOptionType.Channel,
                     channel_types: [ChannelType.GuildText]
@@ -27,16 +27,16 @@ export default class Lock extends Command {
         const channel = interaction.options.getChannel('channel') as TextChannel || interaction.channel;
         const bot = interaction.guild?.members.me?.permissions;
 
-        if (!bot?.has(new Lock(this.client).default_member_permissions)) return await interaction.reply({ content: `${inlineCode(`❌`)} I don't have sufficient permissions to perform this action. Missing Permissions: ${inlineCode(`ManageChannels`)}`, ephemeral: false });
+        if (!bot?.has(new Unlock(this.client).default_member_permissions)) return await interaction.reply({ content: `${inlineCode(`❌`)} I don't have sufficient permissions to perform this action. Missing Permissions: ${inlineCode(`ManageChannels`)}`, ephemeral: false });
 
         await channel.permissionOverwrites.edit(`${interaction.guild?.roles.everyone.id}`, {
-            SendMessages: false,
-            AddReactions: false,
-            CreatePrivateThreads: false,
-            CreatePublicThreads: false,
-            SendMessagesInThreads: false
+            SendMessages: null,
+            AddReactions: null,
+            CreatePrivateThreads: null,
+            CreatePublicThreads: null,
+            SendMessagesInThreads: null
         });
 
-        await interaction.reply({ content: `${inlineCode(`✔️`)} The channel ${channel} has been successfully locked!`, ephemeral: true });
+        await interaction.reply({ content: `${inlineCode(`✔️`)} The channel ${channel} has been successfully unlocked!`, ephemeral: true });
     }
 }
