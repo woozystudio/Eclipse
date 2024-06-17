fa7c5f21957219090eedd5367ff5852b91d7b9df
Woozy's Studio <woozysstudio23@gmail.com>
2024-06-17 16:44:30 -0600

    feat: add new emoji codes

--- enums/Case.ts
+++ enums/Case.ts
@@ -1,6 +1,6 @@
 enum Case {
-    Error = "\`❌\`",
-    Success = "\`✔️\`"
+    Error = "<:crossmark:1252391779454554122>",
+    Success = "<:checkmark:1252391778133348452>"
 }
 
 export default Case;
