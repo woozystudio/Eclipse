cccc2b71e655c88d51507eeabb31e060b1d1459c
Woozy's Studio <woozysstudio23@gmail.com>
2024-06-19 07:18:21 -0600

    chore(i18n): language command

---
+++ commands/configuration/LanguageCommand.ts
@@ -0,0 +1,48 @@
+import { ApplicationCommandOptionType, ChannelType, ChatInputCommandInteraction, PermissionFlagsBits, inlineCode } from "discord.js";
+import Command from "../../class/Command";
+import Eclipse from "../../class/Eclipse";
+import Category from "../../enums/Category";
+import { LocaleParam } from "../../types/LocaleParam";
+import LanguageConfig from "../../database/LanguageConfig";
+import Case from "../../enums/Case";
+
+export default class Language extends Command {
+    constructor(client: Eclipse) {
+        super(client, {
+            name: "language",
+            description: "Configure the bot language on the server.",
+            category: Category.Configuration,
+            userPermissions: PermissionFlagsBits.ManageGuild,
+            options: [
+                {
+                    name: "lang",
+                    description: "Select a language from the list.",
+                    required: true,
+                    type: ApplicationCommandOptionType.String,
+                    choices: [
+                        {
+                            name: "Español México (es-MX)", value: "es-MX"
+                        },
+                        {
+                            name: "English United States (en-US)", value: "en-US"
+                        }
+                    ]
+                }
+            ],
+            development: false,
+            locale: LocaleParam
+        });
+    }
+
+    async Execute(interaction: ChatInputCommandInteraction) {
+        try {
+            const lang = interaction.options.getString('lang') as string;
+    
+            await LanguageConfig.findOneAndUpdate({ GuildID: interaction.guild?.id }, { Language: lang, }, { new: true, upsert: true });
+
+            await interaction.reply({ content: `${Case.Success} The language has been established to ${inlineCode(lang)}`, ephemeral: true });
+        } catch (err) {
+            console.error(err)
+        }
+    }
+}
