0d69a20a731281b897b17970b1e8dc88c7422ea4
Woozy's Studio <woozysstudio23@gmail.com>
2024-06-20 10:38:21 -0600

    feat: add modal interaction to locale param (#5)

--- types/LocaleParam.ts
+++ types/LocaleParam.ts
@@ -1,10 +1,10 @@
-import { AnySelectMenuInteraction, ButtonInteraction, CacheType, ChatInputCommandInteraction, ContextMenuCommandInteraction, Guild } from "discord.js";
+import { AnySelectMenuInteraction, ButtonInteraction, CacheType, ChatInputCommandInteraction, ContextMenuCommandInteraction, Guild, ModalSubmitInteraction } from "discord.js";
 import LanguageConfig from "../database/LanguageConfig";
 import Eclipse from "../class/Eclipse";
 
 export let LocaleParam: string;
 
-export async function fetchLanguage(interaction: ChatInputCommandInteraction | ContextMenuCommandInteraction | ButtonInteraction | AnySelectMenuInteraction) {
+export async function fetchLanguage(interaction: ChatInputCommandInteraction | ContextMenuCommandInteraction | ButtonInteraction | AnySelectMenuInteraction | ModalSubmitInteraction) {
     const data = await LanguageConfig.findOne({ GuildID: interaction.guild?.id });
     if(data && data.Language) {
         return LocaleParam = `${data?.Language}`;
